fb9671221759587bd5053065b8cb58c32ab5a945
vk <vandanrkarakasia@gmail.com>
2024-10-20 09:50:40 +0530

    commit

---
+++ database/connection.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connection=()=>{
+    mongoose.connect(process.env.MONGO_URI,{
+        dbName:"JOB_PORTAL_WITH_AUTOMATION"
+    }).then(()=>{
+        console.log("connected to database");
+
+    }).catch(err=>{
+        console.log(`Some error occoured while connecting to database:${err}`)
+    })
+}
